00f650ce920038fe53cfd5da8a7089ee5cd1416b
PedroGuimaraess <phpguimaraes2005@gmail.com>
2024-09-15 09:05:35 -0300

    Add project

---
+++ atacadinho/src/main/kotlin/com/projeto/atacadinho/AtacadinhoApplication.kt
@@ -0,0 +1,11 @@
+package com.projeto.atacadinho
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtacadinhoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AtacadinhoApplication>(*args)
+}
---
+++ atacadinho/src/test/kotlin/com/projeto/atacadinho/AtacadinhoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projeto.atacadinho
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtacadinhoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
