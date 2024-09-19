3264e9ba4e432ea371dd88a99b074da7114103e1
Matheus Jesus <matheus.arlindo@aluno.faculdadeimpacta.com.br>
2024-09-19 15:38:55 -0300

    teste

--- atacadinho/src/main/front-end/index.js
+++ atacadinho/src/main/front-end/index.js
@@ -1,27 +1,30 @@
-document.getElementById('cadastroForm').addEventListener('submit'), function(event) {
-    event.preventDefault();
+// document.getElementById('cadastroForm').addEventListener('submit'), function(event) {
+//     event.preventDefault();
 
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const userRequestDto = {
-        nome:nome, 
-        email:email, 
-        password:password};
+//     const nome = document.getElementById('nome').value;
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+//     const userRequestDto = {
+//         nome:nome, 
+//         email:email, 
+//         password:password};
     
-    fetch('/cadastrar', {
-        method:'POST', Headers: {
-            'content-type': 'aplication/json',}, 
-            body:JSON.stringify(userRequestDto)})
-            .then(Response=> Response.json())
-            .then(data=>{
-                console.log('Sucesso!',data);
-                window.location.href='./index.html'})
+//     fetch('/cadastrar', {
+//         method:'POST', Headers: {
+//             'content-type': 'aplication/json',}, 
+//             body:JSON.stringify(userRequestDto)})
+//             .then(Response=> Response.json())
+//             .then(data=>{
+//                 console.log('Sucesso!',data);
+//                 window.location.href='./index.html'})
 
-                .catch((error) => {
-                    console.error('Errora:',error);
-                });
+//                 .catch((error) => {
+//                     console.error('Errora:',error);
+//                 });
 
 
 
-}
+// }
+
+
+const cadastrar = documentgetElementById()
