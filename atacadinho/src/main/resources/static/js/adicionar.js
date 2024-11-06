70417cf8dd5286024060761e624cdb58c352a8e0
Guilherme Cutieri <cutieriguilherme@gmail.com>
2024-11-05 23:14:01 -0300

    adicionar

--- atacadinho/src/main/resources/static/js/adicionar.js
+++ atacadinho/src/main/resources/static/js/adicionar.js
@@ -1,36 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buyButtons = document.querySelectorAll('.add-button');
-
-    buyButtons.forEach(button => {
-        button.addEventListener('click', async (event) => {
-            const productCard = event.target.closest('.product-card');
-            const productName = productCard.querySelector('h2').innerText;
-            const quantityInput = productCard.querySelector('input[type="number"]');
-            const quantity = quantityInput.value;
-
-            const productRequestDto = {
-                name: productName,
-                quantidade: parseInt(quantity)
-            };
-
-            try {
-                const response = await fetch('http://localhost:8090/produto/adicionar', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(productRequestDto)
-                });
-
-                if (response.ok) {
-                    alert(`Produto ${productRequestDto.name} Compra com sucesso! Quantidade: ${productRequestDto.quantidade}`);
-                } else {
-                    alert('Erro ao cadastrar produto');
-                }
-            } catch (error) {
-                console.error('Erro ao fazer requisição:', error);
-                alert(`Erro ao conectar com o servidor: ${error.message}`);
-            }
-        });
-    });
+    listUsers();
 });
+
+async function listUsers() {
+    try {
+        const response = await fetch("http://localhost:8090/produto/listproduct");
+        if (!response.ok) {
+            throw new Error('Erro ao buscar usuários');
+        }
+
+        const users = await response.json();
+        const tableBody = document.getElementById('user-table-body');
+        //tableBody.innerHTML = ''; // Limpa a tabela antes de inserir os dados
+
+        users.forEach(user => {
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.cpf_cnpj}</td>
+                <td>
+                    <button class="table-button" onclick="redirectToUpdate(${user.id})">Atualizar</button>
+                </td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+function redirectToUpdate(id) {
+    window.location.href = `http://localhost:3000/atualizar?id=${id}`;
+}
