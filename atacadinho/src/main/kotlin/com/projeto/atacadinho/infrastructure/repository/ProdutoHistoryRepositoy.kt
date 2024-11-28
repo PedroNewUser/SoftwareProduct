d09609612e50477815999816fd3cc48ac5c4aaa8
PedroNewUser <phpguimaraes2005@gmail.com>
2024-11-27 23:11:43 -0300

    Add transactional

--- atacadinho/src/main/kotlin/com/projeto/atacadinho/infrastructure/repository/ProdutoHistoryRepositoy.kt
+++ atacadinho/src/main/kotlin/com/projeto/atacadinho/infrastructure/repository/ProdutoHistoryRepositoy.kt
@@ -2,13 +2,11 @@ package com.projeto.atacadinho.infrastructure.repository
 
 import com.projeto.atacadinho.domain.model.ProdutoHistory
 import jakarta.transaction.Transactional
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.util.Optional
 
 
 @Repository
